7be4e0d7c6ad9c3f23259d99199f7cfd5575a268
Stormir <rhodesst@oregonstate.edu>
2025-11-11 23:49:07 -0800

    wired up auth to view logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,28 @@ import React from "react";
 import Home from "./pages/Home.tsx";
 import Landing from "./pages/Landing.tsx";
 
+import { useSession } from "./api/useSession"; 
 import assert from "./utils/assert.tsx";
 
 type PageEnum = "Home" | "Landing";
 
 const App = () => {
   const [page, setPage] = React.useState<PageEnum>("Landing");
+  
+  // Supabase session tracking
+  const { session, loading } = useSession();
+
+   React.useEffect(() => {
+    if (session && page !== "Home") {
+      // If users logged in, go to Home
+      setPage("Home");          
+    } else if (!session && page !== "Landing") {
+      // if users logged out, go to Landing
+      setPage("Landing");        
+    }
+  }, [session, page]);
+
+  if (loading) return <p>Loading...</p>;
 
   switch (page) {
     case "Home":
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,17 +1,17 @@
 import { supabase } from "../client";
-// auth supabase signup + error
+
 export async function signUp(email: string, password: string) {
   const { data, error } = await supabase.auth.signUp({ email, password });
   if (error) console.error("Sign-up failed:", error.message);
   return { data, error };
 }
-// auth supabase signin + error
+
 export async function signIn(email: string, password: string) {
   const { data, error } = await supabase.auth.signInWithPassword({ email, password });
   if (error) console.error("Sign-in failed:", error.message);
   return { data, error };
 }
-// // auth supabase signout + error 
+
 export async function signOut() {
   const { error } = await supabase.auth.signOut();
   if (error) console.error("Sign-out failed:", error.message);
